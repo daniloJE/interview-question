bfcc87cc3c8101d6b7acb01fd33ba8269abbacb4
daniloJE <danidaniposao@gmail.com>
2020-06-26 20:18:22 +0200

    interview question

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'data-binding1';
+  serverElements = [];
+  activeServerElements = [];
+  newServerName = [];
+  completedServerElements = [];
+  newServerContent = '';
+  active = '';
+  index = 1;
+
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent,
+      active: this.active
+    });
+    this.index = 1;
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+
+  addActive(): any {
+    this.activeServerElements = this.serverElements.filter((v, i, a) => a.indexOf(v) === i && v.active === 'active');
+    this.index = 2;
+    return this.activeServerElements;
+  }
+  completed(): any {
+    this.completedServerElements = this.serverElements.filter((value, index, array) => array.indexOf(value) === index && value.active === 'completed');
+    this.index = 4;
+    return this.completedServerElements;
+  }
+  all(): string[]{
+    this.index = 3;
+    return this.serverElements;
+  }
+
 }
